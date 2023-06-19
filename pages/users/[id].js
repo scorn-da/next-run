d2c265ea32530f88f1a9f3248a73d3e7ec5efd97
Дмитрий Варганов <varganovda@gmail.com>
2023-06-19 14:25:35 +0300

    feat(ui): added a user data fetching

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { useRouter } from "next/router";
 
-export default function () {
-  const { query } = useRouter();
+export default function ({ user }) {
   return (
       <div>
-        <h1>Пользователь c id {query.id}</h1>
+        <h1>Пользователь {user.name} c id {user.id}</h1>
       </div>
   );
 };
+
+export async function getServerSideProps({ params }) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
+  const user = await res.json();
+  return {
+    props: {
+      user,
+    },
+  }
+}
